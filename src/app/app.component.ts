ddc36e611d4e2af613c4cea6940754f708e9de72
Andrzej Malik <andrzejkom2016@gmail.com>
2020-08-25 11:53:56 +0200

    chapter8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl} from '@angular/forms';
+import {filter, debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <h1>Moja czwarta aplikacja Angular(Observables)</h1>
+  <input class="form-control" type="search" [formControl]="searchControl">
+  `
 })
 export class AppComponent {
-  title = 'Justyna';
+  searchControl = new FormControl();
+  constructor(){
+    this.searchControl.valueChanges.pipe(filter(text => text.length >= 3), debounceTime(400), distinctUntilChanged())
+    .subscribe(value => {
+      console.log(value);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
